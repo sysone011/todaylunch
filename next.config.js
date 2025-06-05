9bd2844665bc4867435bd3f50c25efa358bb03c3
sysone011 <sys784@naver.com>
2025-06-05 15:16:23 +0900

    Add OpenAI API key to workflow

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '/todaylunch',
-  assetPrefix: '/todaylunch/',
+  basePath: '',
+  assetPrefix: '',
   trailingSlash: true,
 }
 
