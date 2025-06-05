24d60d65e549a876c19cd71cac159efc5d3cb239
sysone011 <sys784@naver.com>
2025-06-05 15:02:03 +0900

    Update Next.js config for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+    unoptimized: true,
   },
+  basePath: '/todaylunch',
+  assetPrefix: '/todaylunch/',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig 
